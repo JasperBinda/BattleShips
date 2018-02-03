d90d09107e2048a58949e758a3f8828868b0a468
Jasper Binda <jasper.binda@youngcolfield.nl>
2018-02-03 11:27:20 +0100

    new version

--- Battle.java
+++ Battle.java
@@ -12,40 +12,49 @@ public class Battle{
         p2.name = "Player2";
         p2.CreateFleet();
 
-        Player currentPlayer = p1;
-        Player waitingPlayer = p2;
+        Player[] players = new Player[]{p1,p2} ;
+        Shot shot = new Shot();
         while(playingGame) {
             String result = "You missed.";
-            System.out.println(currentPlayer.name + " please enter coordinates to shoot."); //coordinates a1, A1, etc.
-            String input = scanner.next();
-            char[] shot_xy = input.toLowerCase().toCharArray();
-            int xs = shot_xy[0] - 97; //transfer a ascii value to int 0, etc.
-            int ys = shot_xy[1] - 49; //transfer 1 ascii value to int 0, etc.
+            System.out.println(players[0].name + " please enter coordinates to shoot."); //coordinates a1, A1, etc.
 
-            if(currentPlayer.DidIShootHereBefore(xs,ys)){
+            String input = scanner.next();
+            shot.FireShot(input);
+
+            if(players[0].DidIShootHereBefore(shot.x,shot.y)){
                 result = "You already shot here, try again...";
             }else{
-                for (Boat b : waitingPlayer.fleet){
-                    if(b.AmIHit(xs,ys)){
+                for (Boat b : players[1].fleet){
+                    if(b.AmIHit(shot.x,shot.y)){
                         result = "Hit a boat!";
                         if (b.DidISink()) {
                             result = "Boat has sunk.";
-                            playingGame = !(waitingPlayer.EndGame());
+                            playingGame = !(players[1].EndGame());
                         }
                         break;
                     }
                 }
-                currentPlayer.AddShot(xs,ys);
-                Player[] Players = SwitchTurns(currentPlayer, waitingPlayer);
-                currentPlayer = Players[0];
-                waitingPlayer = Players[1];
+                players[0].AddShotToSea(shot.x,shot.y);
+                players = SwitchTurns(players, players[0]);
             }
             System.out.println(result);
         }
-        System.out.println(waitingPlayer.name + " won the game!"); //players switch before this line is called
+        System.out.println(players[1].name + " won the game!"); //players switch before this line is called
     }
-    static Player[] SwitchTurns(Player c, Player w){
-        return new Player[]{w,c};
+    static Player[] SwitchTurns(Player[] plays, Player p0){
+        plays[0] = plays[1];
+        plays[1] = p0;
+        return plays;
+    }
+}
+
+abstract class Sea{
+    int[][] field = new int[5][5];
+    void AddShotToSea(int x, int y){
+        field[x][y] = 1;
+    }
+    boolean DidIShootHereBefore(int x, int y){
+        return (field[x][y] == 1);
     }
 }
 
@@ -55,14 +64,13 @@ class Player extends Sea{
     int destroyedboats;
     void CreateFleet(){
         Boat boat1 = new Boat();
-        boat1.x = new int[] {0,0,0};
-        boat1.y = new int[] {0,1,2}; // boat on a1, a2, a3
+        boat1.xc = new int[] {0,0,0};
+        boat1.yc = new int[] {0,1,2}; // boat on a1, a2, a3
 
         Boat boat2 = new Boat();
-        boat2.x = new int[] {3,4};
-        boat2.y = new int[] {4,4}; // boat on d5, e5
+        boat2.xc = new int[] {3,4};
+        boat2.yc = new int[] {4,4}; // boat on d5, e5
         fleet = new Boat[]{boat1, boat2};
-
     }
     boolean EndGame(){
         destroyedboats += 1;
@@ -70,24 +78,14 @@ class Player extends Sea{
     }
 }
 
-abstract class Sea{
-    int[][] field = new int[5][5];
-    void AddShot(int x, int y){
-        field[x][y] = 1;
-    }
-    boolean DidIShootHereBefore(int x, int y){
-        return (field[x][y] == 1);
-    }
-}
-
 class Boat {
-    int[] x;
-    int[] y;
+    int[] xc;
+    int[] yc;
     int totalhits;
     boolean AmIHit(int x_shot, int y_shot) {
         boolean hit = false;
-        for (int i = 0; i < x.length; i++) {
-            if (x_shot == x[i] && y_shot == y[i]) {
+        for (int i = 0; i < xc.length; i++) {
+            if (x_shot == xc[i] && y_shot == yc[i]) {
                 hit = true;
                 totalhits += 1;
             }
@@ -95,7 +93,16 @@ class Boat {
         return hit;
     }
     boolean DidISink(){
-        return (totalhits == x.length);
+        return (totalhits == xc.length);
     }
 }
 
+class Shot{
+    int x;
+    int y;
+    void FireShot(String coord){
+        char[] shot_xy = coord.toLowerCase().toCharArray();
+        x = shot_xy[0] - 97; //transfer a ascii value to int 0, etc.
+        y = shot_xy[1] - 49; //transfer 1 ascii value to int 0, etc.
+    }
+}
